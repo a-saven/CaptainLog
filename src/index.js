9ccdcc5d85cfdd08df8a915365f3621fbce86668
Anton Savenkov <e2h3in@gmail.com>
2019-11-07 19:39:47 +0100

    Init

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+});
+
+client
+  .query({
+    query: gql`
+      {
+       getBooks {
+        title
+      }
+      getAuthors {
+        name
+      }
+    }`
+  })
+  .then(result => console.log(result));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
