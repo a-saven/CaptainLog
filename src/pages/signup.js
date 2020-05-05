2fa50c0f8f35cdec80085e3bc61fccfab6d2a0ea
Anton Savenkov <e2h3in@gmail.com>
2020-05-05 17:44:24 +0200

    Hash router

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -148,7 +148,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/CaptainLog/login" variant="body2">
+              <Link href="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
